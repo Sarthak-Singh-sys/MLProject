35fe1bb89748818e4bad9c1222bf3032c8a3b7b5
Sarthak Singh <sarthak.singh1199@gmail.com>
2025-02-20 00:09:48 +0530

    Changes in data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,11 @@
+import sys
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from src.exception import CustomException
+from src.logger import logger
+
